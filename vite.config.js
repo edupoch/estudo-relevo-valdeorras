3f9b7cedd9d02ca9ffcfdbcac8215d5cbb25301b
edupoch <edupoch@gmail.com>
2025-02-17 12:43:47 +0100

    Corriximos configuración do build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from "vite";
+import { fileURLToPath } from "node:url";
+import { dirname, resolve } from "node:path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/estudo-relevo-valdeorras",
   assetsInclude: ["modelos/*.tif"],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        ascii: resolve(__dirname, "estudos/ascii/index.html"),
+        normais: resolve(__dirname, "estudos/normais/index.html"),
+        semitonos: resolve(__dirname, "estudos/semitonos/index.html"),
+      },
+    },
+  },
 });
